a714a3c9e18edc96e92c875a3a1fabf200992751
earlrodson <earlrodson@gmail.com>
2025-02-11 12:58:36 +0800

    add ecosystem config

--- Narra-FE/ecosystem.config.js
+++ Narra-FE/ecosystem.config.js
@@ -1,10 +1,9 @@
 module.exports = {
     apps: [
-      {
+        {
         name: "narrafe",
         script: "docker",
         args: "run -d --rm -p 3000:3000 -v $(pwd):/app narrafe"
-      }
+        }
     ]
 };
-  
